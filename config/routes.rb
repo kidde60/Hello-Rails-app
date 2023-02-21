beb428be274f859b242fdfc418671ceeb1dad70b
kidde60 <kgeorgewilliam60@gmail.com>
2023-02-21 18:41:55 +0300

    Add hello rails and root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'pages/hello'
+
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'pages#hello'
 end
